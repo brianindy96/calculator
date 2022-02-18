7d58ac448a35ea2c2a158f7a6a6273a3df9cee52
Brian Garton <gartonb@gmail.com>
2022-02-19 00:33:05 +0100

    AC DEL Default added

--- app.js
+++ app.js
@@ -1,3 +1,39 @@
+let display = document.querySelector(".display");
+
+let btns = Array.from(document.querySelectorAll(".btn"));
+
+// To use map() btns is a Nodelist. Must be changed to an array first.
+
+btns.map( button => button.addEventListener('click', (e) =>{
+        // console.log(e);
+        // console.log(e.target);
+        // console.log(e.target.innerText);
+        switch(e.target.innerText){
+            case "AC":
+                    display.innerText = '';
+                    break;
+            case "DEL":
+                    if(display.innerText){
+                        display.innerText = display.innerText.slice(0,-1);
+                    }
+                    break;
+            
+            
+            
+            
+            
+            
+            default: 
+                    display.innerHTML += e.target.innerHTML;
+                
+            
+        }
+    })
+);
+
+
+
+
 function add(a,b){
     return a+b;
 }
@@ -12,4 +48,8 @@ function multiply(a,b){
 
 function divide(a,b){
     return a/b;
+}
+
+function operate(){
+
 }
